f25194aeb884439906d79eda32b3cc97c62e2a4c
dubilyer <edaorik@gmail.com>
2017-09-19 10:54:20 +0300

    v1

--- src/test/java/learnAppium/AppTest.java
+++ src/test/java/learnAppium/AppTest.java
@@ -1,24 +1,23 @@
 package learnAppium;
 
 import io.appium.java_client.AppiumDriver;
-import org.junit.After;
+import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.net.MalformedURLException;
+
 import static java.lang.Thread.sleep;
 
 /**
  * Unit test for simple App.
  */
 public class AppTest {
-    AppiumDriver driver;
+    static AppiumDriver driver;
     @Before
     public void prepareDevice() throws MalformedURLException {
        driver =  (new AppiumSetup()).getDriver();
@@ -26,14 +25,15 @@ public class AppTest {
 
     @Test
     public void go() throws InterruptedException {
-            WebDriverWait wait = (new WebDriverWait(driver, 5));
+        sleep(5000);
+        WebDriverWait wait = (new WebDriverWait(driver, 5));
             WebElement element = driver.findElement(By.id("btn_left"));
             wait.until(ExpectedConditions.visibilityOf(element));
             element.click();
     }
 
-    @After
-    public void CloseDevice() throws InterruptedException {
+    @AfterClass
+    public static void CloseDevice() throws InterruptedException {
         sleep(1000);
         driver.quit();
     }
